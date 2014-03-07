5c638c0b2e76cafaf6e52c6ad5e7f198a62f5786
ZhangXector <ZhangXector@gmail.com>
2014-03-07 15:15:51 -0700

    Building matrix and printing functions

--- MatrixRotation.cpp
+++ MatrixRotation.cpp
@@ -7,16 +7,66 @@ using namespace std;
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
+// Build Matrix Function
+// Creates a matrix of given size
+// and fills with sequential integers
+// Params: Size of the matrix
+// Returns: 2D vector matrix
+vector< vector<int> > buildMatrix(int matrixSize)
+{
+	// Initialize 2-dimensional vector matrix
+	vector< vector<int> > matrix(matrixSize, vector<int>(matrixSize));
+
+	// Value to add to a cell in the matrix
+	int value = 1;
+
+	// Loops through width and height of matrix
+	// adds values and increments value
+	for(int i = 0; i < matrixSize; ++i)
+	{
+		for(int j = 0; j < matrixSize; ++j)
+		{
+			matrix[i][j] = value;
+			value++;
+		}
+	}
+
+	// return matrix
+	return matrix;
+}
+
+// Print Matrix Function
+// Prints all values of the matrix
+// Params: The matrix to print
+void printMatrix(vector< vector<int> >& matrix)
+{
+	for(int i = 0; i < matrix.size(); ++i)
+	{
+		cout<<"\n";
+		for(int j = 0; j < matrix.size(); ++j)
+		{
+			cout<<matrix[i][j]<<"\t";
+		}
+	}
+}
+
+// Main Function
+// Takes an argument for matrix size
+// creates a matrix and rotates it clockwise
+// and counter-clockwise
 int main(int argc, char* argv[])
 {
 	int matrixSize;
 	
+	// Get size of matrix from command line arguments
 	if(argc == 1)
 	{
+		// Default 5
 		matrixSize = 5;
 	}
 	else if(argc == 2)
 	{
+		// Parse string to int value for passed in size
 		stringstream convert(argv[1]);
 		if(!(convert >> matrixSize))
 		{
@@ -24,27 +74,22 @@ int main(int argc, char* argv[])
 			return EXIT_FAILURE;
 		}
 	}
+	// Error if more than 1 argument
 	else
 	{
 		cout<<"Too many arguments. Please use matrix_rotation <size>.\n";
 		return EXIT_FAILURE;
 	}
 
-	cout<<"Matrix size -> "<<matrixSize;
-	vector< vector<int> > matrix;
+	// Output matrix size
+	cout<<"Matrix size -> "<<matrixSize<<"\n";
+	
+	// Build matrix
+	vector< vector<int> > matrix = buildMatrix(matrixSize);
 
-	/*
-	int i, j;
+	// Print matrix
+	printMatrix(matrix);
 
-	for(i = 0; i < matrixSize; ++i)
-	{
-		cout<<"\n";
-		for (j = 0; j < matrixSize; ++j)
-		{
-			cout<<matrix[i][j]<<"\t";
-		}
-	}
-	*/
-
+	// Successfully exit
 	return EXIT_SUCCESS;
 }
--- makefile
+++ makefile
@@ -1,18 +1,18 @@
 all: matrix-rotation
 
 run: release
-	./matrix-rotation
+	./matrix-rotation.o
 
 debug: clean matrix-rotation-debug
-	gdb matrix-rotation
+	gdb matrix-rotation.o
 
 release: clean matrix-rotation
 
 matrix-rotation-debug: MatrixRotation.cpp
-	g++ MatrixRotation.cpp -g -o matrix-rotation
+	g++ MatrixRotation.cpp -g -o matrix-rotation.o
 
 matrix-rotation: MatrixRotation.cpp
-	g++ MatrixRotation.cpp -o matrix-rotation
+	g++ MatrixRotation.cpp -o matrix-rotation.o
 
 clean:
-	rm -f matrix-rotation
+	rm -f matrix-rotation.o
