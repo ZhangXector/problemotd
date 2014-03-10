39bf416ee0ba8102a764cce8b228cf70477d0a05
ZhangXector <ZhangXector@gmail.com>
2014-03-10 13:27:59 -0600

    Setting up folder for Vigenere Cipher

---
+++ VigenereCipher/VigenereCipher.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+int main(int argc, char* argv[])
+{
+
+	// New line, because aesthetics
+	cout << "\n";
+
+	// Wait for user input before closing window
+	cin.get();
+
+	// Successfully exit
+	return EXIT_SUCCESS;
+}
